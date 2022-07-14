7b5dd3114e622772c95b8486bd9ccd235d98d257
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-07-13 23:59:51 -0700

    Intial commit

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,37 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
-router.get('/', (req, res) => {
-  res.send('User route');
-});
+const User = require('../models/User');
+  
+  // Update user
+  router.put("/:id", async (req, res) => {
+      if(req.body.userId === req.params.id || req.user.isAdmin){
+       if(req.body.password){
+        try {
+          const salt = await bcrypt.genSalt(10);
+          req.body.password = await bcrypt.hash(req.body.password, salt);
+        } catch(err){
+          return res.status(500).json(err);
+        }
+       }
+
+       try {
+          const user = await User.findByIdAndUpdate(req.params.id, { $set: req.body });
+          res.status(200).json("Account has been updated...");
+       } catch(err){
+
+       }
+      } else {
+        return res.status(403).json('You can only update your account...');
+      }
+  });
+
+  // Delete user
+
+  // Get user
+
+  // Follow user
+
+  // unFollow user
 
 module.exports = router;
