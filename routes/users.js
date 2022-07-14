3d8a87bfd9c2965ead420c13b4c06f8b29ae2a32
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-07-14 00:24:16 -0700

    Initial commit

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
   
   // Update user
   router.put("/:id", async (req, res) => {
-      if(req.body.userId === req.params.id || req.user.isAdmin){
+      if(req.body.userId === req.params.id || req.body.isAdmin){
        if(req.body.password){
         try {
           const salt = await bcrypt.genSalt(10);
@@ -19,7 +19,8 @@ const User = require('../models/User');
           const user = await User.findByIdAndUpdate(req.params.id, { $set: req.body });
           res.status(200).json("Account has been updated...");
        } catch(err){
-
+        return res.status(500).json(err);
+        console.log("Server error...");
        }
       } else {
         return res.status(403).json('You can only update your account...');
@@ -27,8 +28,30 @@ const User = require('../models/User');
   });
 
   // Delete user
-
+  router.delete("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+     try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("Account have been deleted...");
+     } catch(err){
+      return res.status(500).json(err);
+      console.log("Server error...");
+     }
+    } else {
+      return res.status(403).json('You can only delete your account...');
+    }
+});
   // Get user
+ router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch(err){
+    res.status(500).json(err);
+    console.log('Sever error...');
+  }
+ })
 
   // Follow user
 
