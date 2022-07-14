72947f4cb271422e27982238ce6809c325eb4ee5
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-07-13 23:22:30 -0700

    Initial commit

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,32 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch(err){
-   console.log(err);
+   res.status(500).json(err);
+   console.log("Server error");
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email
+    });
+
+    if(!user){
+      res.status(404).json("User not found...");
+    } 
+
+   const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+   if(!validPassword){
+    res.status(400).json("Password do not match...");
+   };
+
+   res.status(200).json(user);
+
+  } catch(err) {
+    res.status(500).json(err);
+    console.log("Server error");
   }
 })
 
