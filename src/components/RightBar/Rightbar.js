fda2b7cf34a014d8339865b7817f95c182ba0bae
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-08-13 00:10:01 -0700

    Initial commit

--- src/components/RightBar/Rightbar.js
+++ src/components/RightBar/Rightbar.js
@@ -1,5 +1,8 @@
 import './Rightbar.scss';
 
+import { Users } from '../DummyData/DummyData';
+import Online from '../Online/Online';
+
 export default function Rightbar() {
   return (
     <div className='rightbar'>
@@ -13,61 +16,11 @@ export default function Rightbar() {
         <img className='rightbarAd' src='/assets/ad.png' alt='img' />
         <h4 className='rightbarTitle'>Online friends!</h4>
         <ul className='rightbarFriendList'>
-          <li className="rightbarFriend">
-            <div className='rightbarProfileImgContainer'>
-             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
-             <span className='rightbarOnline'></span>
-            </div>
-            <span className='rightbarUserName'>John Carter!</span>
-          </li>
-
-          <li className="rightbarFriend">
-            <div className='rightbarProfileImgContainer'>
-             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
-             <span className='rightbarOnline'></span>
-            </div>
-            <span className='rightbarUserName'>John Carter!</span>
-          </li>
-
-          <li className="rightbarFriend">
-            <div className='rightbarProfileImgContainer'>
-             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
-             <span className='rightbarOnline'></span>
-            </div>
-            <span className='rightbarUserName'>John Carter!</span>
-          </li>
-
-          <li className="rightbarFriend">
-            <div className='rightbarProfileImgContainer'>
-             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
-             <span className='rightbarOnline'></span>
-            </div>
-            <span className='rightbarUserName'>John Carter!</span>
-          </li>
-
-          <li className="rightbarFriend">
-            <div className='rightbarProfileImgContainer'>
-             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
-             <span className='rightbarOnline'></span>
-            </div>
-            <span className='rightbarUserName'>John Carter!</span>
-          </li>
-
-          <li className="rightbarFriend">
-            <div className='rightbarProfileImgContainer'>
-             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
-             <span className='rightbarOnline'></span>
-            </div>
-            <span className='rightbarUserName'>John Carter!</span>
-          </li>
-
-          <li className="rightbarFriend">
-            <div className='rightbarProfileImgContainer'>
-             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
-             <span className='rightbarOnline'></span>
-            </div>
-            <span className='rightbarUserName'>John Carter!</span>
-          </li>
+         {
+          Users.map((u) => (
+            <Online key={u.id} user={u} />
+          ))
+         }
         </ul>
       </div>
     </div>
