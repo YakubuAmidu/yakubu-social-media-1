da6f9071d5a2f44091ca2d55eb47e370964950e1
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-08-12 22:57:09 -0700

    Initial commit

---
+++ DummyData.js
@@ -0,0 +1,103 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: '/assets/persons/profile-1.png',
+    userName: 'Yakubu Amidu'
+  },
+  {
+    id: 2,
+    profilePicture: '/assets/persons/profile-2.png',
+    userName: 'John Doe'
+  },
+  {
+    id: 3,
+    profilePicture: '/assets/persons/profile-3.png',
+    userName: 'Yakubu amidu'
+  },
+  {
+    id: 4,
+    profilePicture: '/assets/persons/profile-4.png',
+    userName: 'Kwame inkrumah'
+  },
+  {
+    id: 5,
+    profilePicture: '/assets/persons/profile-5.png',
+    userName: 'Asamoah Joe'
+  },
+  {
+    id: 6,
+    profilePicture: '/assets/persons/profile-6.png',
+    userName: 'Princesses Duafe'
+  },
+  {
+    id: 7,
+    profilePicture: '/assets/persons/profile-7.png',
+    userName: 'Samanta kudjoe'
+  },
+];
+
+export const Post = [
+  {
+    id: 1,
+    desc: 'Love for all, Hatred for non!',
+    photo: '/assets/post/dessert-1.png',
+    date: '5 min ago',
+    userId: 1,
+    like: 32,
+    comment: 9
+  },
+  {
+    id: 2,
+    desc: 'Having fun is way to stay happy!',
+    photo: '/assets/post/drinks-1.png',
+    date: '15 min ago',
+    userId: 2,
+    like: 100,
+    comment: 10
+  },
+  {
+    id: 3,
+    desc: 'Drink wisely, and stay healthy!',
+    photo: '/assets/post/drinks-2.png',
+    date: '30 min ago',
+    userId: 3,
+    like: 200,
+    comment: 20
+  },
+  {
+    id: 4,
+    desc: 'Eating and drinking',
+    photo: '/assets/post/food-1.png',
+    date: '1 hour min ago',
+    userId: 4,
+    like: 500,
+    comment: 30
+  },
+  {
+    id: 5,
+    desc: 'Ahhh!, am staving. I havent eaten since morning',
+    photo: '/assets/post/food-2.png',
+    date: '1 hour 30 min ago',
+    userId: 5,
+    like: 150,
+    comment: 25
+  },
+  {
+    id: 6,
+    desc: 'At home chilling, and relaxing!',
+    photo: '/assets/post/home-1.png',
+    date: '50 min ago',
+    userId: 6,
+    like: 320,
+    comment: 90
+  },
+  {
+    id: 7,
+    desc: 'More than ever. Luxury vacation',
+    photo: '/assets/post/vacation-1.png',
+    date: '40 min ago',
+    userId: 7,
+    like: 800,
+    comment: 50
+  },
+]
--- src/components/RightBar/Rightbar.js
+++ src/components/RightBar/Rightbar.js
@@ -2,6 +2,74 @@ import './Rightbar.scss';
 
 export default function Rightbar() {
   return (
-    <div className='rightbar'>Rightbar</div>
+    <div className='rightbar'>
+      <div className='rightbarWrapper'>
+        <div className='birthdayContainer'>
+          <img className='birthdayImg' src='/assets/gift.png' alt='img' />
+          <span className='birthdayText'>
+            <b>Polar foster </b> and <b>3 other friends </b>have birthday today!
+          </span>
+        </div>
+        <img className='rightbarAd' src='/assets/ad.png' alt='img' />
+        <h4 className='rightbarTitle'>Online friends!</h4>
+        <ul className='rightbarFriendList'>
+          <li className="rightbarFriend">
+            <div className='rightbarProfileImgContainer'>
+             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
+             <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUserName'>John Carter!</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className='rightbarProfileImgContainer'>
+             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
+             <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUserName'>John Carter!</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className='rightbarProfileImgContainer'>
+             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
+             <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUserName'>John Carter!</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className='rightbarProfileImgContainer'>
+             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
+             <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUserName'>John Carter!</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className='rightbarProfileImgContainer'>
+             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
+             <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUserName'>John Carter!</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className='rightbarProfileImgContainer'>
+             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
+             <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUserName'>John Carter!</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className='rightbarProfileImgContainer'>
+             <img className="rightbarProfileImg" src="/assets/persons/profile-7.png" alt="img" />
+             <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUserName'>John Carter!</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
